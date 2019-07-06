8bca9088f9e3d8abc0de05766e37040947605a8c
mavo97 <mavodeveloper@gmail.com>
2019-07-06 13:05:45 -0500

    aproximity for template

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+  .ng-invalid.ng-touched:not(form) {
+    border: 1px solid red
+  }
+  `]
+})
+export class TemplateComponent {
+
+  usuario:Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: '',
+    sexo: 'Hombre',
+    acepta: false
+  };
+
+  paises = [{
+    codigo: 'MX',
+    nombre: 'México'
+  }, {
+    codigo: 'USA',
+    nombre: 'Estados Unidos'
+  }
+];
+  sexos: string[] = [
+    'Hombre', 'Mujer'
+  ];
+
+  constructor() { }
+
+
+
+  guardar(forma: NgForm) {
+    console.log('Formulario posteado');
+    console.log('ngForm', forma);
+    console.log('Valor forma', forma.value);
+
+    console.log('Usuario', this.usuario);
+  }
+
+}
